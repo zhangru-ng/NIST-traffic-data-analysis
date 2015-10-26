8172e7989f3c07fe71076570c1fb735db55e4dea
Rui Zhang <ruizhang1011@ufl.edu>
2015-10-25 20:20:00 -0400

    Sort output by lane_id and measurement start complete

--- src/main/java/edu/ufl/ds/sort/SortDriver.java
+++ src/main/java/edu/ufl/ds/sort/SortDriver.java
@@ -1,4 +1,4 @@
-package edu.ufl.ds.consistence;
+package edu.ufl.ds.sort;
 
 import java.io.IOException;
 import java.net.URI;
@@ -31,88 +31,90 @@ public class SortDriver {
 
     public static class LaneIdAndTimeComparator extends WritableComparator {
 
-	protected LaneIdAndTimeComparator() {
-	    super(Text.class, true);
-	}
+        protected LaneIdAndTimeComparator() {
+            super(Text.class, true);
+        }
 
-	@SuppressWarnings("rawtypes")
-	@Override
-	public int compare(WritableComparable w1, WritableComparable w2) {
-	    /*
-	     * parts[0]: lane_id
-	     * parts[1]: measurement start
-	     */
-	    String[] parts1 = ((Text) w1).toString().split(",");
-	    String[] parts2 = ((Text) w2).toString().split(",");
-	    int lane_id1 = Integer.parseInt(parts1[0]);
-	    int lane_id2 = Integer.parseInt(parts2[0]);
-	    try {
-		Date date1 = new SimpleDateFormat("HH:mm:ss").parse(parts1[1].split(" ")[2]);
-		Date date2 = new SimpleDateFormat("HH:mm:ss").parse(parts2[1].split(" ")[2]);
-		int cmp = date1.compareTo(date2);
-		if (cmp != 0) {
-		    return cmp;
-		} else {
-		    return Integer.compare(lane_id1, lane_id2);
-		}
-	    } catch (ParseException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	    }
-	    return 1;
-	}
+        @SuppressWarnings("rawtypes")
+        @Override
+        public int compare(WritableComparable w1, WritableComparable w2) {
+            /*
+             * parts[0]: lane_id
+             * parts[1]: measurement_start
+             */
+            String[] pair1 = ((Text) w1).toString().split(",");
+            String[] pair2 = ((Text) w2).toString().split(",");
+            int lane_id1 = Integer.parseInt(pair1[0]);
+            int lane_id2 = Integer.parseInt(pair2[0]);
+            try {
+                Date date1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(pair1[1]);
+                Date date2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(pair2[1]);
+                int cmp = date1.compareTo(date2);
+                if (cmp != 0) {
+                    return cmp;
+                } else {
+                    return Integer.compare(lane_id1, lane_id2);
+                }
+            } catch (ParseException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            return 1;
+        }
     }
 
     public static void sortDriver(Path inputPath)
-	    throws ClassNotFoundException, IOException, InterruptedException, URISyntaxException {
-	Configuration conf = new Configuration();
+            throws ClassNotFoundException, IOException, InterruptedException, URISyntaxException {
+        Configuration conf = new Configuration();
 
-	conf.set("mapreduce.output.textoutputformat.separator", ",");
-	Job job = Job.getInstance(conf);
-	job.setJarByClass(SortDriver.class);
+        conf.set("mapreduce.output.textoutputformat.separator", ",");
+        Job job = Job.getInstance(conf);
+        job.setJarByClass(SortDriver.class);
 
-	job.setMapperClass(SortMapper.class);
-	job.setOutputKeyClass(Text.class);
-	job.setOutputValueClass(Text.class);
-	job.setReducerClass(SortReducer.class);
+        job.setMapperClass(SortMapper.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        job.setSortComparatorClass(LaneIdAndTimeComparator.class);
+        job.setReducerClass(SortReducer.class);
+        job.setNumReduceTasks(1);
 
-	String cleanTestName = inputPath.getName();
-	Path tmpPath = new Path(tmp + cleanTestName);
+        String cleanTestName = inputPath.getName();
+        Path tmpPath = new Path(tmp + cleanTestName);
 
-	FileSystem fs = FileSystem.get(new URI(outBucket), conf);
-	if (fs.exists(tmpPath)) {
-	    fs.delete(tmpPath, true);
-	}
-	Path resultPath = new Path(result + resultPrefix + cleanTestName);
+        FileSystem fs = FileSystem.get(new URI(outBucket), conf);
+        if (fs.exists(tmpPath)) {
+            fs.delete(tmpPath, true);
+        }
+        Path resultPath = new Path(result + resultPrefix + cleanTestName);
 
-	FileInputFormat.addInputPath(job, inputPath);
-	job.setInputFormatClass(TextInputFormat.class);
+        FileInputFormat.addInputPath(job, inputPath);
+        job.setInputFormatClass(TextInputFormat.class);
 
-	job.setOutputFormatClass(TextOutputFormat.class);
-	FileOutputFormat.setOutputPath(job, tmpPath);
+        job.setOutputFormatClass(TextOutputFormat.class);
+        FileOutputFormat.setOutputPath(job, tmpPath);
 
-	job.waitForCompletion(true);
-	FileUtil.copyMerge(fs, tmpPath, fs, resultPath, true, conf, "");
+        job.waitForCompletion(true);
+        FileUtil.copyMerge(fs, tmpPath, fs, resultPath, false, conf, "");
     }
     /*
      * args[0]: Cleaning test directory path
      * args[1]: Output bucket(directory) path
      */
     public static void main(String[] args) throws Exception {
-	Configuration conf = new Configuration();
-	String[] paths = new GenericOptionsParser(conf, args).getRemainingArgs();
+        Configuration conf = new Configuration();
+        String[] paths = new GenericOptionsParser(conf, args).getRemainingArgs();
 
-	Path inputPath = new Path(paths[0]);
-	outBucket = paths[1];
-	tmp = outBucket + "tmp/";
-	result = outBucket + "result/";
+        Path inputPath = new Path(paths[0]);
+        outBucket = paths[1];
+        tmp = outBucket + "tmp/";
+        result = outBucket + "result/";
 
-	FileSystem fs = FileSystem.get(new URI(paths[1]), conf);
-	// run on all cleaning_test file, run each file separately to preserve file name in result
-	RemoteIterator<LocatedFileStatus> fileIterator = fs.listFiles(inputPath, false);
-	while (fileIterator.hasNext()) {
-	    LocatedFileStatus stat = fileIterator.next();
-	    SortDriver.sortDriver(stat.getPath());
-	}
+        FileSystem fs = FileSystem.get(new URI(paths[1]), conf);
+        // run on all cleaning_test file, run each file separately to preserve file name in result
+        RemoteIterator<LocatedFileStatus> fileIterator = fs.listFiles(inputPath, false);
+        while (fileIterator.hasNext()) {
+            LocatedFileStatus stat = fileIterator.next();
+            SortDriver.sortDriver(stat.getPath());
+        }
     }
 }
--- src/main/java/edu/ufl/ds/sort/SortReducer.java
+++ src/main/java/edu/ufl/ds/sort/SortReducer.java
@@ -1,19 +1,18 @@
-package edu.ufl.ds.consistence;
+package edu.ufl.ds.sort;
 
 import java.io.IOException;
 
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class SortReducer extends Reducer<Text, Text, Text, Text> {
-    private Text outputKey = new Text();
-    private Text outputVal = new Text();
+public class SortReducer extends Reducer<Text, Text, Text, NullWritable> {
+
     @Override
     public void reduce(Text key, Iterable<Text> values, Context context)
 	    throws IOException, InterruptedException {
-	// mean and std cleaning can be done in each group
-	for (Text v : values) {
-	    context.write(key, v);
-	}
+    	for (Text v : values) {
+    	    context.write(v, null);
+    	}
     }
 }
