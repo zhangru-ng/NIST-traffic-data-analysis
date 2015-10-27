b5bd6a01833ef580474ff3e4ff5c2d1bd5eff460
Rui Zhang <rayzhang1011@gmail.com>
2015-10-27 19:29:25 -0400

    Parallel sort

--- src/main/java/edu/ufl/ds/sort/SortDriver.java
+++ src/main/java/edu/ufl/ds/sort/SortDriver.java
@@ -1,16 +1,17 @@
 package edu.ufl.ds.sort;
 
+import java.io.DataInput;
+import java.io.DataOutput;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.FileUtil;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
@@ -19,53 +20,82 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.mapreduce.lib.partition.InputSampler;
+import org.apache.hadoop.mapreduce.lib.partition.TotalOrderPartitioner;
 
 import edu.ufl.ds.Cleaning;
 
 public class SortDriver {
 
+    public class LaneIdAndTimePair implements WritableComparable<LaneIdAndTimePair> {
+	    private IntWritable lane_id;
+	    private LongWritable timestamp;
+
+	    public LaneIdAndTimePair(int lane_id, long timestamp) {
+	        this.lane_id = new IntWritable(lane_id);
+	        this.timestamp = new LongWritable();
+	    }
+
+	    @Override
+	    public int compareTo(LaneIdAndTimePair lt) {
+	        int cmp = timestamp.compareTo(lt.timestamp);
+	        if(0 != cmp)
+	            return cmp;
+	        return lane_id.compareTo(lt.lane_id);
+	    }
+
+	    @Override
+	    public void readFields(DataInput in) throws IOException {
+		lane_id.readFields(in);
+		timestamp.readFields(in);
+	    }
+	    @Override
+	    public void write(DataOutput out) throws IOException {
+		lane_id.write(out);
+		timestamp.write(out);
+	    }
+	}
+
     public static class LaneIdAndTimeComparator extends WritableComparator {
 
         protected LaneIdAndTimeComparator() {
-            super(Text.class, true);
+            super(LaneIdAndTimePair.class, true);
+        }
+
+        @Override
+        public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
+            long time1 = readLong(b1, s1+4);
+            long time2 = readLong(b2, s2+4);
+            int cmp = (time1 < time2) ? -1 : (time1 == time2) ? 0 : 1;
+            if(0 != cmp)
+                return cmp;
+
+            int lane_id1 = readInt(b1, s1);
+            int lane_id2 = readInt(b2, s2);
+            return (lane_id1 < lane_id2) ? -1 : (lane_id1 == lane_id2) ? 0 : 1;
         }
 
         @SuppressWarnings("rawtypes")
         @Override
         public int compare(WritableComparable w1, WritableComparable w2) {
-            /*
-             * parts[0]: lane_id
-             * parts[1]: measurement_start
-             */
-            String[] pair1 = ((Text) w1).toString().split(",");
-            String[] pair2 = ((Text) w2).toString().split(",");
-            int lane_id1 = Integer.parseInt(pair1[0]);
-            int lane_id2 = Integer.parseInt(pair2[0]);
-            try {
-                Date date1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(pair1[1]);
-                Date date2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(pair2[1]);
-                int cmp = date1.compareTo(date2);
-                if (cmp != 0) {
-                    return cmp;
-                } else {
-                    return Integer.compare(lane_id1, lane_id2);
-                }
-            } catch (ParseException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
+            LaneIdAndTimePair pair1 = (LaneIdAndTimePair) w1;
+            LaneIdAndTimePair pair2 = (LaneIdAndTimePair) w2;
+            int cmp = Long.compare(pair1.timestamp.get(), pair2.timestamp.get());
+            if (cmp != 0) {
+                return cmp;
+            } else {
+                return Integer.compare(pair1.lane_id.get(), pair2.lane_id.get());
             }
-            return 1;
         }
     }
 
-    public static void sort(String filename)
+    @SuppressWarnings("unchecked")
+    public static void sort(String input, String partition, String tmp)
             throws ClassNotFoundException, IOException, InterruptedException, URISyntaxException {
         Configuration conf = new Configuration();
         FileSystem fs = FileSystem.get(new URI(Cleaning.outBucket), conf);
 
-        String inputDir = Cleaning.outBucket + "nearby/" + filename.substring(7).replaceAll(".csv", "");
-
-        String outputName = filename.substring(7).replaceAll(".csv", "_nist7.txt").replaceAll("test", "subm");
+        String outputName = input.substring(7).replaceAll(".csv", "_nist7.txt").replaceAll("test", "subm");
         Path outPath = new Path(Cleaning.outBucket + "result/" + outputName);
         if (fs.exists(outPath)) {
             return;
@@ -78,24 +108,41 @@ public class SortDriver {
         job.setMapperClass(SortMapper.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
+
+        job.setPartitionerClass(TotalOrderPartitioner.class);
         job.setSortComparatorClass(LaneIdAndTimeComparator.class);
         job.setReducerClass(SortReducer.class);
-        job.setNumReduceTasks(1);
 
-        Path tmpPath = new Path(Cleaning.sortTmp);
-
+        Path tmpPath = new Path(tmp);
         if (fs.exists(tmpPath)) {
             fs.delete(tmpPath, true);
         }
 
+        String inputDir = Cleaning.outBucket + "nearby/" + input.substring(7).replaceAll(".csv", "");
         FileInputFormat.addInputPath(job, new Path(inputDir));
         job.setInputFormatClass(TextInputFormat.class);
+
+        Path partPath = new Path(partition);
+        if (fs.exists(partPath)) {
+            fs.delete(partPath, true);
+        }
+
+        Path partitionFile = new Path(partPath, "partitioning");
+        TotalOrderPartitioner.setPartitionFile(conf, partitionFile);
+
+        double pcnt = 10.0;
+        int numReduceTasks = 10;
+        int numSamples = numReduceTasks;
+        int maxSplits = numReduceTasks - 1;
+
+        @SuppressWarnings("rawtypes")
+	InputSampler.Sampler sampler = new InputSampler.RandomSampler(pcnt, numSamples, maxSplits);
+        InputSampler.writePartitionFile(job, sampler);
 
         job.setOutputFormatClass(TextOutputFormat.class);
         FileOutputFormat.setOutputPath(job, tmpPath);
 
         job.waitForCompletion(true);
-
         FileUtil.copyMerge(fs, tmpPath, fs, outPath, true, conf, "");
     }
 
--- src/main/java/edu/ufl/ds/sort/SortMapper.java
+++ src/main/java/edu/ufl/ds/sort/SortMapper.java
@@ -12,8 +12,21 @@ public class SortMapper extends Mapper<LongWritable, Text, Text, Text> {
     @Override
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String[] parts = value.toString().split(",", 3);
-        outputKey.set(parts[0] + "," + parts[1]);
+        outputKey.set(parts[0] + "," + convert(parts[1].substring(8, parts[1].length() - 3)));
         outputVal.set(value);
         context.write(outputKey, outputVal);
+    }
+
+    public long convert(String time) {
+	String[] dayParts = time.split(" ");
+	int day = Integer.parseInt(dayParts[0]);
+	String[] milliParts = time.split(".");
+	int millis = milliParts.length > 1 ? Integer.parseInt(milliParts[1]) : 0;
+	String[] timeParts = milliParts[0].split(":");
+	int hour = Integer.parseInt(timeParts[0]);
+	int minute = Integer.parseInt(timeParts[1]);
+	int second = Integer.parseInt(timeParts[2]);
+	long timestamp = (((day * 24L + hour) * 60 + minute) * 60 + second) * 1000 + millis;
+	return timestamp;
     }
 }
