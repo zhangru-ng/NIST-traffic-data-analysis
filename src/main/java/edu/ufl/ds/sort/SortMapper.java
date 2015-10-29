d0dad1c45d90b8c38584d5bf3fd5e7b44073165e
Rui Zhang <rayzhang1011@gmail.com>
2015-10-29 11:27:51 -0400

    Modified timestamp to use Microsecond

--- src/main/java/edu/ufl/ds/sort/SortMapper.java
+++ src/main/java/edu/ufl/ds/sort/SortMapper.java
@@ -23,13 +23,13 @@ public class SortMapper extends Mapper<LongWritable, Text, LaneIdAndTimePair, Te
 	String[] milliParts = dayParts[1].split("\\.");
 	int millis = 0;
 	if (milliParts.length > 1) {
-	    millis = Integer.parseInt(String.format("%-3s", milliParts[1]).replace(' ', '0'));
+	    millis = Integer.parseInt(String.format("%-6s", milliParts[1]).replace(' ', '0'));
 	}
 	String[] timeParts = milliParts[0].split(":");
 	int hour = Integer.parseInt(timeParts[0]);
 	int minute = Integer.parseInt(timeParts[1]);
 	int second = Integer.parseInt(timeParts[2]);
-	long timestamp = (((day * 24L + hour) * 60 + minute) * 60 + second) * 1000 + millis;
+	long timestamp = (((day * 24L + hour) * 60L + minute) * 60L + second) * 1000000L + millis;
 	return timestamp;
     }
 }
